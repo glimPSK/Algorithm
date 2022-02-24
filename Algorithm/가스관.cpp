c8d662f2b7bc08a7d9dd58d4030b639f4a418c06
{박상경} <tltmzh3@gmail.com>
2022-02-24 11:32:09 +0900

    가스관 solved

--- Algorithm/가스관.cpp
+++ Algorithm/가스관.cpp
@@ -1,111 +1,125 @@
-#include <iostream>
-#include <cstdlib>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-#define SIZE 30
+#define SIZE 26
 
-int r{}, c{};
+int R{}, C{}, dir{};
 char map[SIZE][SIZE]{};
-bool visited[SIZE][SIZE]{};
-int mr{}, mc{}, zr{}, zc{};
-int br{}, bc{};
-char block{};
 
-int dy[] = { 0,0,1,-1 };
-int dx[] = { 1,-1,0,0 };
+vector <pair <int, int>> v{};
 
-void init() {
-    memset(visited, false, sizeof(visited));
+// ↑ 0  ↓ 1  ← 2  → 3
+int dy[] = { -1, 1, 0, 0 };
+int dx[] = { 0, 0, -1, 1 };
+
+void result(int y, int x) {
+	cout << y << " " << x << " ";
+	bool visited[4]{};
+
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny <= 0 || ny > R || nx <= 0 || nx > C) continue;
+
+		if (map[ny][nx] != '.') {
+			if (i == 0 && (map[ny][nx] == '|' || map[ny][nx] == '+' || map[ny][nx] == '1' || map[ny][nx] == '4'))
+				visited[i] = true;
+			else if (i == 1 && (map[ny][nx] == '|' || map[ny][nx] == '+' || map[ny][nx] == '2' || map[ny][nx] == '3'))
+				visited[i] = true;
+			else if (i == 2 && (map[ny][nx] == '-' || map[ny][nx] == '+' || map[ny][nx] == '1' || map[ny][nx] == '2'))
+				visited[i] = true;
+			else if (i == 3 && (map[ny][nx] == '-' || map[ny][nx] == '+' || map[ny][nx] == '3' || map[ny][nx] == '4'))
+				visited[i] = true;
+		}
+	}
+
+	if (visited[0] && visited[1] && visited[2] && visited[3]) cout << "+";
+	else if (visited[0] && visited[1]) cout << "|";
+	else if (visited[2] && visited[3]) cout << "-";
+	else if (visited[1] && visited[3]) cout << "1";
+	else if (visited[0] && visited[3]) cout << "2";
+	else if (visited[0] && visited[2]) cout << "3";
+	else if (visited[1] && visited[2]) cout << "4";
+	return;
 }
 
-void input_data() {
-    cin >> r >> c;
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            cin >> map[i][j];
-            if (map[i][j] == 'M') {
-                mr = i;
-                mc = j;
-            }
-            else if (map[i][j] == 'Z') {
-                zr = i;
-                zc = j;
-            }
-        }
-    }
+void dfs(int y, int x, int d) {
+	if (map[y][x] == '.') {
+		result(y, x);
+		return;
+	}
+
+	int ny = y + dy[d];
+	int nx = x + dx[d];
+
+	if (d == 0) { //위
+		if (map[ny][nx] == '|' || map[ny][nx] == '+') d = 0;
+		else if (map[ny][nx] == '1') d = 3;
+		else if (map[ny][nx] == '4') d = 2;
+	}
+
+	else if (d == 1) {	// 아래
+		if (map[ny][nx] == '|' || map[ny][nx] == '+') d = 1;
+		else if (map[ny][nx] == '2') d = 3;
+		else if (map[ny][nx] == '3') d = 2;
+	}
+
+	else if (d == 2) {	// 왼
+		if (map[ny][nx] == '-' || map[ny][nx] == '+') d = 2;
+		else if (map[ny][nx] == '1') d = 1;
+		else if (map[ny][nx] == '2') d = 0;
+	}
+
+	else if (d == 3) {	// 오
+		if (map[ny][nx] == '-' || map[ny][nx] == '+') d = 3;
+		else if (map[ny][nx] == '3') d = 0;
+		else if (map[ny][nx] == '4') d = 1;
+	}
+
+	dfs(ny, nx, d);
+	return;
 }
 
-// 블록이 있으면 방향대로 dfs 진행
-//        없으면 방향에서 갈 수 있는 것만 . 대신 바꿔서 진행
-void dfs(int y, int x, bool flag) {
-    visited[y][x] = true;
+void check_map() {
+	for (int i = 0; i < 4; i++) {
+		int ny = v.front().first + dy[i];
+		int nx = v.front().second + dx[i];
 
-    if (y == zr && x == zc) {
-        return;
-    }
+		if (ny <= 0 || ny > R || nx <= 0 || nx > C) continue;
 
-    // flag 는 빈칸이 1개 지나쳤다는 표시
-    if (flag) {
-        int cnt = 0;
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= r || nx >= c)   continue;
-            if (map[ny][nx] == '.') {
-                cnt++;
-                continue;
-            }
-        }
-        if (cnt == 4)   return;
-    }
-
-    // 현재 위치에 있는 기호에 따라 탐색
-    // 맵변경 -> dfs -> 원상복귀
-    if (map[y][x] == '|') {
-        // 위 + 아래
-        // 다음올 수 있는 기호는 + 2 3
-    }
-    else if (map[y][x] == '-') {
-        // 왼 + 오른
-        // 다음올 수 있는 기호는 + 1 2 3 4
-    }
-    else if (map[y][x] == '+') {
-        // 4방향 탐색
-        // 다음올 수 있는 기호는 | - + 1 2 3 4
-    }
-    else if (map[y][x] == '1') {
-        // 오른 + 아래
-        // 다음올 수 있는 기호는 | - + 2 3 4
-    }
-    else if (map[y][x] == '2') {
-        // 위 + 오른 
-    }
-    else if (map[y][x] == '3') {
-        // 위 + 왼
-    }
-    else if (map[y][x] == '4') {
-        // 왼 + 아래
-    }
-}
-
-void solution() {
-    /*
-    -> 빈칸에 7개를 모두 놔본다
-    BigO = 25 * 25 * 7
-    M -> 4방향에서 길이 있다면 -> 가보기 -> dfs
-                길이 없다면 -> 7방향 모두 가보기 -> dfs
-    */
-    dfs(mr, mc, false);
+		if (map[ny][nx] != '.') {
+			if (ny == v.front().first) {
+				if (nx == v.front().second - 1)
+					dir = 2;
+				else if (nx == v.front().second + 1)
+					dir = 3;
+			}
+			else if (nx == v.front().second) {
+				if (ny == v.front().first - 1)
+					dir = 0;
+				else if (ny == v.front().first + 1)
+					dir = 1;
+			}
+		}
+	}
 }
 
 int main() {
+	cin >> R >> C;
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 'M')
+				v.push_back(make_pair(i, j));
+		}
+	}
 
-    init();
+	check_map();
 
-    input_data();
+	dfs(v.front().first, v.front().second, dir);
 
-    solution();
-
-    cout << br + 1 << bc + 1 << block << endl;
-    return 0;
+	return 0;
 }
