909a2ea3d334a0c6f3f5c0c06c63f697ef89c5d7
{박상경} <tltmzh3@gmail.com>
2022-03-02 10:24:50 +0900

    미네랄 commit

---
+++ Algorithm/미네랄.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int r, c;
+
+
+int main() {
+	/*
+	1. 왼 오 순서로 던짐
+	2. 1번 던지고나서 밑으로 내려올 수 있으면 내려옴
+	3. 상하좌우로 연결되어 있지 않고, 밑에 .이 존재하면 떨어짐
+	*/
+
+
+	return 0;
+}
