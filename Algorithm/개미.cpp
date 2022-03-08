b2d95b12acecea36cd822a2328f4911b06528701
{박상경} <tltmzh3@gmail.com>
2022-03-08 20:14:10 +0900

    [Fix] 3048 개미

--- Algorithm/개미.cpp
+++ Algorithm/개미.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 using namespace std;
-#define SIZE 50
 
-int r{}, c{};
-char map[SIZE][SIZE]{};
-int t{};
+int N1 = 0;
+int N2 = 0;
+int T;
 
 int main() {
-	cin >> r >> c;
+	cin >> N1 >> N2;
+
+	string right, left;
+	cin >> right;
+	cin >> left;
+	cin >> T;
+	reverse(right.begin(), right.end());
+	string ant = right + left;
+	int time = 0;
+	while (time < T) {
+		for (int i = 0; i < N1 + N2 - 1; i++) {
+			if (right.find(ant[i]) != string::npos && left.find(ant[i + 1]) != string::npos) {
+				swap(ant[i], ant[i + 1]);
+				i++;
+			}
+		}
+		time += 1;
+	}
+	cout << ant;
 
 	return 0;
 }
