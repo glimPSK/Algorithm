f2bfc6c8dcaf95043f7e8e0e398b765ad11e60cc
{박상경} <tltmzh3@gmail.com>
2022-04-10 19:21:14 +0900

    [Commit] 21609 상어중학교

--- Algorithm/상어중학교.cpp
+++ Algorithm/상어중학교.cpp
@@ -1,61 +1,69 @@
+
+// 참고 : https://gist.github.com/jaimemin/6f5b60d1f2ccf780eb0f588949b21ce0#file-cpp
+// # 21609 상어중학교
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
+#include <iomanip>
 using namespace std;
 
-const int SIZE = 20;
 const int INF = 987654321;
 const int MAX = 20;
 const int EMPTY = -2;
 const int BLACK = -1;
 const int RAINBOW = 0;
 
-typedef struct {
+typedef struct
+{
 	int y, x;
-} S_POS;
+} Coord;
 
-typedef struct {
-	S_POS baseBlock;
-	S_POS arr[MAX * MAX];
+typedef struct
+{
+	Coord baseBlock;
+	Coord arr[MAX * MAX];
 	int blockGroupSize;
 	int rainbowCnt;
-} S_BLACK;
+} BlockGroup;
 
-int N{}, M{};
+typedef struct
+{
+	int y, x;
+} Dir;
+
+Dir moveDir[4] = { {-1, 0}, {1, 0}, {0, 1}, {0, -1} };
+
+int N, M;
 int board[MAX][MAX];
 bool visited[MAX][MAX];
 
-int dy[] = { 0,0,1,-1 };
-int dx[] = { 1,-1,0,0 };
-
-void input_data() {
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> board[i][j];
-		}
-	}
-}
-
-void gravity() {
+void gravity(void)
+{
 	// 중력
-	for (int i = N - 2; i >= 0; i--) { 
-		for (int j = 0; j < N; j++)	{
-			if (board[i][j] <= BLACK) {
+	for (int i = N - 2; i >= 0; i--)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (board[i][j] <= BLACK)
+			{
 				continue;
 			}
 
 			int y = i;
 
-			while (true) {
-				if (y == N)	{
+			while (1)
+			{
+				if (y == N)
+				{
 					break;
 				}
 
 				int cur = board[y][j];
 				int next = board[y + 1][j];
 
-				if (next != EMPTY) {
+				if (next != EMPTY)
+				{
 					break;
 				}
 
@@ -67,43 +75,52 @@ void gravity() {
 	}
 }
 
-void rotateBoard() {
+void rotateBoard(void)
+{
 	int tempBoard[MAX][MAX];
 
-	for (int i = 0; i < N; i++)	{
-		for (int j = 0; j < N; j++) {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
 			tempBoard[i][j] = board[i][j];
 		}
 	}
 
-	for (int i = 0; i < N; i++)	{
-		for (int j = 0; j < N; j++) {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
 			board[N - (j + 1)][i] = tempBoard[i][j];
 		}
 	}
 }
 
-S_BLACK getS_BLACK(int y, int x, int color) {
-	S_BLACK tempS_BLACK;
-	tempS_BLACK.baseBlock = { INF, INF };
-	tempS_BLACK.rainbowCnt = 0;
+BlockGroup getBlockGroup(int y, int x, int color)
+{
+	BlockGroup tempBlockGroup;
+	tempBlockGroup.baseBlock = { INF, INF };
+	tempBlockGroup.rainbowCnt = 0;
 
 	int baseColor = board[y][x] == RAINBOW ? color : board[y][x];
 
 	int tempBoard[MAX][MAX];
 
-	for (int i = 0; i < N; i++)	{
-		for (int j = 0; j < N; j++)	{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
 			tempBoard[i][j] = board[i][j] == RAINBOW ? color : board[i][j];
 		}
 	}
 
-	queue<S_POS> q;
+	queue<Coord> q;
 	q.push({ y, x });
 	visited[y][x] = true;
 	int arrIdx = 0;
 
-	while (!q.empty()) {
+	while (!q.empty())
+	{
 		int curY = q.front().y;
 		int curX = q.front().x;
 		q.pop();
@@ -111,86 +128,112 @@ S_BLACK getS_BLACK(int y, int x, int color) {
 		// 무지개블록 수
 		if (board[curY][curX] == RAINBOW)
 		{
-			tempS_BLACK.rainbowCnt++;
+			tempBlockGroup.rainbowCnt++;
 		}
 
 		// 기준 블록은 무지개 블록이 아닌 블록 중에서 행의 번호가 가장 작은 블록,
 		// 그러한 블록이 여러개면 열의 번호가 가장 작은 블록이다.
 		if (board[curY][curX] != RAINBOW
-			&& (tempS_BLACK.baseBlock.y > curY
-				|| (tempS_BLACK.baseBlock.y == curY && tempS_BLACK.baseBlock.x > curX)))
+			&& (tempBlockGroup.baseBlock.y > curY
+				|| (tempBlockGroup.baseBlock.y == curY && tempBlockGroup.baseBlock.x > curX)))
 		{
-			tempS_BLACK.baseBlock = { curY, curX };
+			tempBlockGroup.baseBlock = { curY, curX };
 		}
 
 		// 블록을 그룹에 추가
-		tempS_BLACK.arr[arrIdx++] = { curY, curX };
+		tempBlockGroup.arr[arrIdx++] = { curY, curX };
 
-		for (int k = 0; k < 4; k++) {
-			int nextY = curY + dy[k];
-			int nextX = curX + dx[k];
+		for (int k = 0; k < 4; k++)
+		{
+			int nextY = curY + moveDir[k].y;
+			int nextX = curX + moveDir[k].x;
 
-			if (nextY < 0 || nextY >= N || nextX < 0 || nextX >= N || tempBoard[nextY][nextX] <= BLACK) continue;
+			// 갈 수 없는 블록
+			if (nextY < 0 || nextY >= N || nextX < 0 || nextX >= N || tempBoard[nextY][nextX] <= BLACK)
+			{
+				continue;
+			}
+
 			// 같은 색깔이 아니라면
-			if (tempBoard[nextY][nextX] != baseColor) continue;
-			if (visited[nextY][nextX]) continue;
+			if (tempBoard[nextY][nextX] != baseColor)
+			{
+				continue;
+			}
+
+			if (visited[nextY][nextX])
+			{
+				continue;
+			}
 
 			visited[nextY][nextX] = true;
 			q.push({ nextY, nextX });
 		}
 	}
 
-	tempS_BLACK.blockGroupSize = arrIdx;
-	if (tempS_BLACK.rainbowCnt == arrIdx) {
-		tempS_BLACK.blockGroupSize = 0;
+	tempBlockGroup.blockGroupSize = arrIdx;
+
+	if (tempBlockGroup.rainbowCnt == arrIdx)
+	{
+		tempBlockGroup.blockGroupSize = 0;
 	}
 
-	return tempS_BLACK;
+	return tempBlockGroup;
 }
 
-int getScore() {
-	S_BLACK blockGroup;
+int getScore(void)
+{
+	BlockGroup blockGroup;
 	blockGroup.rainbowCnt = 0;
 	blockGroup.blockGroupSize = 0;
 	blockGroup.baseBlock = { INF, INF };
 
-	for (int y = 0; y < N; y++)	{
-		for (int x = 0; x < N; x++)	{
-			if (board[y][x] <= BLACK) {
+	for (int y = 0; y < N; y++)
+	{
+		for (int x = 0; x < N; x++)
+		{
+			if (board[y][x] <= BLACK)
+			{
 				continue;
 			}
 
 			// 무지개 블록을 각 색깔마다 지정하여 진행
-			for (int color = 1; color <= M; color++) {
+			for (int color = 1; color <= M; color++)
+			{
 				memset(visited, false, sizeof(visited));
 
-				S_BLACK tempS_BLACK = getS_BLACK(y, x, color);
+				BlockGroup tempBlockGroup = getBlockGroup(y, x, color);
 
-				if (tempS_BLACK.blockGroupSize < 2) {
+				if (tempBlockGroup.blockGroupSize < 2)
+				{
 					continue;
 				}
 
 				// 크기가 가장 큰 블록 그룹을 찾는다
-				if (tempS_BLACK.blockGroupSize > blockGroup.blockGroupSize) {
-					blockGroup = tempS_BLACK;
+				if (tempBlockGroup.blockGroupSize > blockGroup.blockGroupSize)
+				{
+					blockGroup = tempBlockGroup;
 
 					continue;
 				}
 
 				// 그러한 블록 그룹이 여러 개라면 포함된 무지개 블록의 수가 가장 많은 블록 그룹
-				if (tempS_BLACK.blockGroupSize == blockGroup.blockGroupSize)	{
-					if (tempS_BLACK.rainbowCnt > blockGroup.rainbowCnt) {
-						blockGroup = tempS_BLACK;
+				if (tempBlockGroup.blockGroupSize == blockGroup.blockGroupSize)
+				{
+					if (tempBlockGroup.rainbowCnt > blockGroup.rainbowCnt)
+					{
+						blockGroup = tempBlockGroup;
 
 						continue;
 					}
 
 					// 그러한 블록도 여러개라면 기준 블록의 행이 가장 큰 것을, 그 것도 여러개이면 열이 가장 큰 것을 찾는다
-					if (tempS_BLACK.rainbowCnt == blockGroup.rainbowCnt)	{
-						if (blockGroup.baseBlock.y < tempS_BLACK.baseBlock.y
-							|| (blockGroup.baseBlock.y == tempS_BLACK.baseBlock.y
-								&& blockGroup.baseBlock.x < tempS_BLACK.baseBlock.x)) {
-							blockGroup = tempS_BLACK;
+					if (tempBlockGroup.rainbowCnt == blockGroup.rainbowCnt)
+					{
+						if (blockGroup.baseBlock.y < tempBlockGroup.baseBlock.y
+							|| (blockGroup.baseBlock.y == tempBlockGroup.baseBlock.y
+								&& blockGroup.baseBlock.x < tempBlockGroup.baseBlock.x))
+						{
+							blockGroup = tempBlockGroup;
 						}
 					}
 				}
@@ -199,40 +242,52 @@ int getScore() {
 	}
 
 	// 그룹에 속한 블록의 개수는 2보다 크거나 같아야하며
-	if (blockGroup.blockGroupSize < 2) return 0;
+	if (blockGroup.blockGroupSize < 2)
+	{
+		return 0;
+	}
 
-	for (int i = 0; i < blockGroup.blockGroupSize; i++) {
+	for (int i = 0; i < blockGroup.blockGroupSize; i++)
+	{
 		board[blockGroup.arr[i].y][blockGroup.arr[i].x] = EMPTY;
 	}
+
 	return blockGroup.blockGroupSize * blockGroup.blockGroupSize;
 }
 
-void solution() {
-	/*
-	1) 크기가 가장 큰 블록 그룹을 찾는다. 
-		그러한 블록 그룹이 여러 개라면 포함된 무지개 블록의 수가 가장 많은 블록 그룹, 
-		그러한 블록도 여러개라면 기준 블록의 행이 가장 큰 것을, 그 것도 여러개이면 열이 가장 큰 것을 찾는다.
-	2) 1에서 찾은 블록 그룹의 모든 블록을 제거한다. 
-		블록 그룹에 포함된 블록의 수를 B라고 했을 때, B2점을 획득한다.
-	3) 격자에 중력이 작용한다.
-	4) 격자가 90도 반시계 방향으로 회전한다.
-	5) 다시 격자에 중력이 작용한다.
-	*/
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> board[i][j];
+		}
+	}
+
 	int result = 0;
-	while (true) {
+
+	while (1)
+	{
 		int score = getScore();
-		if (score == 0)	break;
+
+		if (score == 0)
+		{
+			break;
+		}
+
 		result += score;
+
 		gravity();
 		rotateBoard();
 		gravity();
 	}
 
 	cout << result << "\n";
-}
 
-int main() {
-	input_data();
-	solution();
 	return 0;
 }
